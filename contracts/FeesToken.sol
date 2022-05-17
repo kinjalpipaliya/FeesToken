b1eed093168f84b4bbb1ac18989611b26351f2d7
Kinjal Pipaliya <52915562+kinjalpipaliya@users.noreply.github.com>
2022-05-17 20:51:07 +0530

    - Ceated token with fees, burn and blocklist functionality.

---
+++ contracts/FeesToken.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FeesToken is ERC20, Ownable {
+
+    mapping(address => bool) public addressBlacklisted;
+    address public mainFundWallet;
+
+    uint32 public transferFee = 200; // Consumes while transfer of FeesToken. 10 in percent
+    uint32 public burnTokenFee = 300; // Consumes while burn of FeesToken. 10 in percent
+    uint256 public constant FeesCollector = 10000;
+
+    constructor(string memory _name, string memory _symbol, address payable _FTNWallet) ERC20(_name, _symbol) {
+        _mint(msg.sender, 10000000 * 10 ** 18); // 10 million
+        mainFundWallet = _FTNWallet;
+        blacklistAddress(0x6E1c3822c01a87a0090dDB81D5965afE60decb67, true);
+    }
+
+    function _transfer(address from,address to,uint256 totalAmount) internal override {
+        require(!addressBlacklisted[from] && !addressBlacklisted[to], "Address is blacklisted");
+
+        uint256 burnAmount = calculateFeesCollected(totalAmount, burnTokenFee);
+        uint256 transferFeeAmount = calculateFeesCollected(totalAmount, transferFee);
+        uint256 tokensToTransfer = totalAmount - burnAmount - transferFeeAmount; // Final amount after excluding all fees amounts.
+
+        require(burnAmount > 0, "Amount must be more than 0");
+        _burn(from, burnAmount);
+        require(transferFeeAmount > 0, "Amount must be more than 0");
+        super._transfer(from, mainFundWallet, transferFeeAmount); // Transfer fees amount to main wallet.
+        super._transfer(from, to, tokensToTransfer); // Transfer remaining token amount to address
+    }
+
+    function calculateFeesCollected(uint256 _totalAmount, uint32 _totalFees) public pure returns (uint256 totalFeeAmount){
+        totalFeeAmount = (_totalAmount * _totalFees) / FeesCollector;
+    }
+    function blacklistAddress(address account, bool value) public onlyOwner {
+        addressBlacklisted[account] = value;
+    }
+    function changeMainWallet(address payable newWalletAddress) public onlyOwner {
+        mainFundWallet = newWalletAddress;
+    }
+    function transferOwnership(address newOwner) public override onlyOwner {
+        super.transferOwnership(newOwner);
+    }
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const FeesToken = await hre.ethers.getContractFactory("FeesToken");
+  const feesToken = await FeesToken.deploy("FeesToken","FTN","0x3A1428Ff5b5A81AC520Ce9630A4Ff4127ff0a345");
+
+  await feesToken.deployed();
+
+  console.log("FeesToken deployed to:", feesToken.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
